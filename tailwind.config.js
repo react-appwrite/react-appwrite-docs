6a45a5c90d7c7bcc30d8d55df4fad5c8deba1aff
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2023-04-02 11:23:50 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  // purge: ["./**/*.tsx"],
+  mode: "jit",
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./ui/**/*.{js,ts,jsx,tsx}",
+    "./customComponents/**/*.{js,ts,jsx,tsx}",
+    "./_posts/**/*.{md,mdx}",
+  ],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.amber,
+      },
+    },
+  },
+};
